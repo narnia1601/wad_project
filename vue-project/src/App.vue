465010533b473aba0c8bb295b73d55656ed827ed
Aloysius Tan <69228664+narnia1601@users.noreply.github.com>
2022-10-08 16:02:56 +0800

    added bootstrap

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,20 +1,9 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Hello World</h1>
+  <button class="btn btn-primary">Primary</button>
 </template>
 
 <style scoped>
