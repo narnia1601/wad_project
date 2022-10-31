fcde79c0f0d5f70cd4128e7052a8c77f2a46ae99
ccjh23 <clovischowjh@gmail.com>
2022-10-31 19:11:01 +0800

    Changed nav links to correct ones for header and footer. Create itinerary page - moved week number above days, fixed days don't tally error

--- vue-project/src/components/Header.vue
+++ vue-project/src/components/Header.vue
@@ -1,14 +1,14 @@
 <template>
   <nav class="navbar navbar-expand-lg bg-light">
     <div class="container-fluid">
-      <a class="navbar-brand" href="/"><img src="../assets/logo.png"></a>
+      <a class="navbar-brand" href="/dashboard"><img src="../assets/logo.png"></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ms-auto">
           <li class="nav-item">
-            <router-link to="/" class="nav-link">Home</router-link>
+            <router-link to="/dashboard" class="nav-link">Home</router-link>
           </li>
           <li class="nav-item">
             <a href="/new" class="nav-link">Create Itinerary</a>
