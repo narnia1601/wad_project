9fbae270d12bfb86352a3830267250923f649222
ccjh23 <clovischowjh@gmail.com>
2022-11-07 20:46:24 +0800

    Added delete an individual attraction functionality to create itinerary page

--- vue-project/src/components/createItinerary/CreateItinerary.vue
+++ vue-project/src/components/createItinerary/CreateItinerary.vue
@@ -66,16 +66,36 @@
             <div class="col-4 mt-4">
                 <ul v-if="itineraryArr != null || itineraryArr[selectedDay - 1].length > 0" class="list-group">
                     <li class="list-group-item" v-for="itinerary in itineraryArr[selectedDay - 1]" :key="itinerary.id">
-                        <div class="row">
+                        <!-- <div class="row">
                             <div class="col text-center">
                                 <h5>{{ itinerary.location }}</h5>
-                                <p v-if="itinerary.description.length > 100">{{ itinerary.description.substring(1,100) + '...' }}</p>
+                                <p v-if="itinerary.description.length > 100">{{ itinerary.description.substring(0,100) + '...' }}</p>
                                 <p v-else>{{ itinerary.description }}</p>
                             </div>
-                            <div class="col text-center w-50">
+                            <div class="col text-center w-50 d-flex align-items-center">
+                                <img id="img" :src="itinerary.image" alt="">
+                            </div>
+                        </div> -->
+
+                        <div class="row">
+                            <div class="col-10 text-center">
+                                <h5>{{ itinerary.location }}</h5>
+                            </div>
+                            <div class="col-2 d-flex justify-content-end">
+                                <svg @click="deleteAttraction(itinerary.location)" xmlns="http://www.w3.org/2000/svg" width="30px" height="30px" preserveAspectRatio="xMidYMid meet" viewBox="0 0 16 16"><path fill="currentColor" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8L4.646 5.354a.5.5 0 0 1 0-.708z"/></svg>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col">
+                                <p v-if="itinerary.description.length > 100">{{ itinerary.description.substring(0,100) + '...' }}</p>
+                                <p v-else>{{ itinerary.description }}</p>
+                            </div>
+                            <div class="col">
                                 <img id="img" :src="itinerary.image" alt="">
                             </div>
                         </div>
+
                     </li>
                 </ul>
             </div>
@@ -284,6 +304,22 @@
                         this.scrollFunction()
                     })
             },
+            deleteAttraction(locationToDelete) {
+                // array
+                let dayItineraryArr = this.itineraryArr[this.selectedDay - 1]
+
+                for (let i=0; i<dayItineraryArr.length; i++) {
+                    let dayItinerary = dayItineraryArr[i]
+
+                    // dayItinerary is an object
+                    // console.log(dayItinerary)
+
+                    if (dayItinerary.location == locationToDelete) {
+                        dayItineraryArr.splice(i, 1)
+                    }
+                }
+                this.itineraryArr[this.selectedDay - 1] = dayItineraryArr
+            }
         },
     }
 </script>
