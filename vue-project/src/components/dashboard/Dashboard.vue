cee685660f88f5f8c328633c8c3e156ee14a8d81
mthj <maxwelltanhuiji@gmail.com>
2022-11-10 17:15:33 +0800

    Added greeting

--- vue-project/src/components/dashboard/Dashboard.vue
+++ vue-project/src/components/dashboard/Dashboard.vue
@@ -1,10 +1,10 @@
 <script setup>
-    import Itineraries from '../Itineraries.vue'
+import Itineraries from '../Itineraries.vue'
 </script>
 
 <template>
     <div class='container-fluid' id="dashboard">
-        <h1>Hello<span v-if="firstName != ''">, {{ getUserName }}</span></h1>
+        <h1>{{greet}}<span v-if="firstName != ''">, {{ getUserName }}</span></h1>
         <div id="background"></div>
         <Itineraries></Itineraries>
     </div>
@@ -17,20 +17,33 @@ export default {
     mounted() {
         this.getUser()
         const cookie = VueCookies.get('id')
-        if(cookie == null){
+        if (cookie == null) {
             this.$router.push('/')
         }
-        if(localStorage.getItem('reloaded')){
+        if (localStorage.getItem('reloaded')) {
             localStorage.removeItem('reloaded')
-        }else{
+        } else {
             localStorage.setItem('reloaded', '1')
             location.reload()
         }
     },
     computed: {
-        getUserName(){
+        getUserName() {
             console.log(this.firstName)
             return this.firstName
+        },
+        greet() {
+            var myDate = new Date();
+            var hrs = myDate.getHours();
+
+            
+
+            if (hrs < 12)
+                return 'Good Morning';
+            else if (hrs >= 12 && hrs <= 17)
+                return 'Good Afternoon';
+            else if (hrs >= 17 && hrs <= 24)
+                return 'Good Evening';
         }
     },
     data() {
@@ -39,28 +52,29 @@ export default {
         }
     },
     methods: {
-        getUser(){
+        getUser() {
             const cookie = VueCookies.get('id')
             const url = this.$link + '/user'
             axios.post(url, {
                 _id: cookie
             })
-            .then(res => {
-                this.firstName = res.data.firstName
-            })
+                .then(res => {
+                    this.firstName = res.data.firstName
+                })
         }
     }
 }
 </script>
 
 <style scoped>
-    #background{
-        background: url('../../assets/individualitinerary-pic1.jpg') no-repeat;
-        background-size: cover;
-        height: 40vh;
-        background-position: center;
-    }
-    #dashboard{
-        padding: 0%;
-    }
+#background {
+    background: url('../../assets/individualitinerary-pic1.jpg') no-repeat;
+    background-size: cover;
+    height: 40vh;
+    background-position: center;
+}
+
+#dashboard {
+    padding: 0%;
+}
 </style>
---
+++ vue-project/src/components/dashboard/Greeting.vue
@@ -0,0 +1,30 @@
+<template>
+    <h1 id="greeting"></h1>
+</template>
+
+
+<script>
+var myDate = new Date();
+var hrs = myDate.getHours();
+
+var greet;
+
+if (hrs < 12)
+  greet = 'Good Morning';
+else if (hrs >= 12 && hrs <= 17)
+  greet = 'Good Afternoon';
+else if (hrs >= 17 && hrs <= 24)
+  greet = 'Good Evening';
+document.getElementById('greeting').innerHTML = greet;
+
+
+
+export default{
+        props: ['Greeting'],
+    }
+</script>
+
+
+
+
+
